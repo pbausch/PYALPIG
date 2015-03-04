3d7e6b7689ea5ba5a2ac2bec7050376c90194c0d
Paul Bausch <pbausch@gmail.com>
2015-03-03 20:24:19 -0800

    kwargs, append sting to output file, poisson disk for random points

--- triangles.py
+++ triangles.py
@@ -6,7 +6,6 @@ import matplotlib.colors as matcolors
 import matplotlib.cm as cmx
 from matplotlib.collections import PatchCollection
 import numpy as np
-from skimage import data
 from skimage import io
 from skimage.color import rgb2gray
 from skimage.draw import polygon, set_color
@@ -15,6 +14,7 @@ from skimage.filter import gaussian_filter
 from skimage.util import img_as_ubyte
 from PIL import Image, ImageOps, ImageDraw, ImageFilter
 from scipy.spatial import Delaunay
+from poisson.poisson_disk import sample_poisson_uniform
 
 def tint_image(src, color="#FFFFFF"):
     src.load()
@@ -23,11 +23,26 @@ def tint_image(src, color="#FFFFFF"):
     result = ImageOps.colorize(gray, (0, 0, 0, 0), color) 
     return result
 
-def process(file,blur,detail,size,trialpha):
+def process(**kwargs):
+	# set default arguments
+	file = kwargs.pop('file', "")
+	append = kwargs.pop('append', "lp")
+	blur = kwargs.pop('blur', 0)
+	detail = kwargs.pop('detail', 1)
+	size = kwargs.pop('size', 1)
+	trialpha = kwargs.pop('trialpha', 1)
+	random = kwargs.pop('random', False)
+	
+	# open the source image
 	img = Image.open(file)
 	img = ImageOps.expand(img,border=20,fill='white')
 	img = ImageOps.expand(img,border=5,fill='black')
 	w, h = img.size
+
+	# uncomment to sharpen image (more points)
+	#img = img.filter(ImageFilter.UnsharpMask(radius=2, percent=150, threshold=3))
+	#img.show()
+	#exit()
 
 	source = img_as_ubyte(img)
 	img1 = rgb2gray(source)
@@ -40,17 +55,20 @@ def process(file,blur,detail,size,trialpha):
 	# blur image (fewer points)
 	if (blur > 0):
 		img1 = gaussian_filter(img1, sigma=blur, multichannel=True)
-	
-	# extract points
-	corners = corner_peaks(corner_fast(img1, detail), min_distance=size)
 
-	pts = np.zeros((len(corners),2))
-	pts[:,0] = corners[:, 1]
-	pts[:,1] = corners[:, 0]
+	# extract or generate points
+	if (random):
+		corners = sample_poisson_uniform(w,h,size,30)
+		pts = np.zeros((len(corners),2))
+		for i in range(len(corners)):
+			x, y = corners[i]
+			pts[i] = [int(x), int(y)]
+	else:
+		corners = corner_peaks(corner_fast(img1, detail), min_distance=size)
+		pts = np.zeros((len(corners),2))
+		pts[:,0] = corners[:, 1]
+		pts[:,1] = corners[:, 0]
 	
-	# uncomment for random points instead (square image)
-	#pts = np.random.random_integers(w, size=(800,2))
-		
 	triangles = Delaunay(pts)
 	
 	# COLOR SELECTION TWEAKS
@@ -84,7 +102,7 @@ def process(file,blur,detail,size,trialpha):
 			G = colors[1] / 255.
 			B = colors[2] / 255.
 		color = [R,G,B]
-		patches.append(plt.Polygon(triangle, fill=True, color=color, alpha=1, ec='none', aa=True))
+		patches.append(plt.Polygon(triangle, fill=True, color=color, alpha=trialpha, ec='none', aa=True))
 
 	ax.imshow(source)
 	ax.axis('off')
@@ -100,18 +118,17 @@ def process(file,blur,detail,size,trialpha):
 	
 	# new file name
 	name = os.path.splitext(file)[0]
-	newname = name + '_lp.png'
+	newname = name + '_' + append + '.png'
 	
 	# save + open file
 	plt.savefig(newname, dpi=170, bbox_inches='tight', pad_inches = 0)
 	command = "open " + newname
 	os.system(command)
 
-# go (file, blur, detail, size, trialpha)
-#
-# a larger blur means less detail
-# a smaller detail number means more detail
+# append is a string to append to the end of the file when saving
+# a larger blur means less detail to extract points from
+# a smaller detail number means more extracted points
 # a smaller size number means smaller triangles
 # setting trialpha to less than 1 means some of the source image will show
-
-process(sys.argv[1],0,1,1,1)
+file = sys.argv[1]
+process(file=file, append="lp", blur=0, detail=1, size=1, trialpha=1, random=False)
